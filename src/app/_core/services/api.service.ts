94762fe66145d93a8e6d5c8773015c12d34885f0
Dominik <spiderqwerty@gmail.com>
2022-10-28 14:08:48 +0200

    changed get method to generic type in api.service.ts

--- src/app/_core/services/api.service.ts
+++ src/app/_core/services/api.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
-import { Fuel } from '../models/fuel.model';
 
 @Injectable({
   providedIn: 'root'
@@ -11,8 +10,8 @@ export class ApiService {
 
   constructor(private http: HttpClient) { }
   
-  get(path: string, data = {}): Observable<Fuel[]> {
-    return this.http.get<Fuel[]>(path, { params: data })
+  get(path: string, data = {}): Observable<any> {
+    return this.http.get(path, { params: data })
       .pipe(catchError(error => throwError(error)));
   }
 }
