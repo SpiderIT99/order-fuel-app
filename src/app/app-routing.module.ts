19914aadd64bb5381f3841123555418846cc8b9c
Dominik <spiderqwerty@gmail.com>
2022-10-31 16:35:37 +0100

    removed unnecessary component 'page-not-found'

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,14 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { OrdersComponent } from './pages/orders/orders.component';
 import { OrderListComponent } from './pages/order-list/order-list.component';
-import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component'
 
 const routes: Routes = [
   { path: '', redirectTo: '/home-page', pathMatch: 'full' },
   { path: 'home-page', component: HomePageComponent },
   { path: 'orders', component: OrdersComponent },
   { path: 'order-list', component: OrderListComponent },
-  { path: '**', component: PageNotFoundComponent }
+  { path: '**', redirectTo: '/home-page' }
 ];
 
 @NgModule({
