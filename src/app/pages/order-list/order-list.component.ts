893f95b3b16224f12d884d6e17676df89049ca6d
Dominik <spiderqwerty@gmail.com>
2022-10-31 23:10:22 +0100

    created a function to retrieve list of orders

--- src/app/pages/order-list/order-list.component.ts
+++ src/app/pages/order-list/order-list.component.ts
@@ -1,8 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { OrderService } from 'src/app/_core/services/order.service';
 
 @Component({
   selector: 'app-order-list',
   templateUrl: './order-list.component.html',
   styleUrls: ['./order-list.component.scss']
 })
-export class OrderListComponent { }
+export class OrderListComponent implements OnInit{
+  public orderListData: any[] = [];
+
+  constructor(private orderService: OrderService) { }
+
+  ngOnInit(): void {
+    this.getOrder();
+  }
+
+  private getOrder(): void {
+    this.orderService.getOrder().subscribe( (response) => {
+      this.orderListData = response;
+      console.log(response)
+    }, error => {
+      console.error("error: ", error);
+    })
+  }
+}
